8652ca3c4ca3d016ed6984e693913ce0f7b8df0f
MinRK <benjaminrk@gmail.com>
2012-10-26 15:34:04 -0700

    don't double DPI across the board

--- retina.py
+++ retina.py
@@ -6,6 +6,7 @@ Usage:  %load_ext retina
 
 import struct
 from base64 import encodestring
+from io import BytesIO
 
 import matplotlib
 from matplotlib.figure import Figure
@@ -19,19 +20,34 @@ def pngxy(data):
     w4h4 = data[ihdr+4:ihdr+12]
     return struct.unpack('>ii', w4h4)
 
+def print_figure(fig, fmt='png', dpi=None):
+    """Convert a figure to svg or png for inline display."""
+    fc = fig.get_facecolor()
+    ec = fig.get_edgecolor()
+    bytes_io = BytesIO()
+    dpi = dpi or matplotlib.rcParams['savefig.dpi']
+    fig.canvas.print_figure(bytes_io, format=fmt, dpi=dpi,
+                            bbox_inches='tight',
+                            facecolor=fc, edgecolor=ec,
+    )
+    data = bytes_io.getvalue()
+    return data
+
 def png2x(fig):
     """render figure to 2x PNG via HTML"""
-    pngbytes = print_figure(fig, 'png')
+    if not fig.axes and not fig.lines:
+        return
+    # double DPI
+    dpi = 2 * matplotlib.rcParams['savefig.dpi']
+    pngbytes = print_figure(fig, fmt='png', dpi=dpi)
     x,y = pngxy(pngbytes)
     x2x = x // 2
     y2x = y // 2
     png64 = encodestring(pngbytes).decode('ascii')
     return u"<img src='data:image/png;base64,%s' width=%i height=%i/>" % (png64, x2x, y2x)
-    
+
 def enable_retina(ip):
     """enable retina figures"""
-    # double DPI, so we don't end up changing the figure size
-    matplotlib.rcParams['savefig.dpi'] = 2 * matplotlib.rcParams['savefig.dpi']
     
     # unregister existing formatter(s):
     png_formatter = ip.display_formatter.formatters['image/png']
