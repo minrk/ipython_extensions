5e01370978c9e3b5c94d69a22c235420e6bce57c
MinRK <benjaminrk@gmail.com>
2013-03-15 22:52:21 -0700

    add nbinput

---
+++ nbinput.py
@@ -0,0 +1,71 @@
+"""
+
+Simple getpass / raw_input workarounds for the IPython notebook using jQueryUI dialogs.
+Not awesome, because they don't *return* the response, they store them in a variable,
+but it should suffice in a few situations while we implement the real thing.
+
+"""
+
+def nbgetpass(prompt="Enter Password", name='passwd'):
+    display_javascript(Javascript("""
+var dialog = $('<div/>').append(
+    $('<input/>')
+    .attr('id', 'password')
+    .attr('name', 'password')
+    .attr('type', 'password')
+    .attr('value', '')
+);
+$(document).append(dialog);
+dialog.dialog({
+    resizable: false,
+    modal: true,
+    title: "%s",
+    closeText: '',
+    buttons : {
+        "Okay": function () {
+            IPython.notebook.kernel.execute(
+                "%s = '" + $("input#password").attr('value') + "'"
+            );
+            $(this).dialog('close');
+            dialog.remove();
+        },
+        "Cancel": function () {
+            $(this).dialog('close');
+            dialog.remove();
+        }
+    }
+});
+""" % (prompt, name)))
+                                  
+def nb_raw_input(prompt, name="raw_input_reply"):
+    display_javascript(Javascript("""
+var dialog = $('<div/>').append(
+    $('<input/>')
+    .attr('id', 'theinput')
+    .attr('value', '')
+);
+$(document).append(dialog);
+dialog.dialog({
+    resizable: false,
+    modal: true,
+    title: "%s",
+    closeText: '',
+    buttons : {
+        "Okay": function () {
+            IPython.notebook.kernel.execute(
+                "%s = '" + $("input#theinput").attr('value') + "'"
+            );
+            $(this).dialog('close');
+            dialog.remove();
+        },
+        "Cancel": function () {
+            $(this).dialog('close');
+            dialog.remove();
+        }
+    }
+});
+""" % (prompt, name)))
+
+def load_ipython_extension(ip):
+    ip.user_ns['nb_raw_input'] = nb_raw_input
+    ip.user_ns['nbgetpass'] = nbgetpass
