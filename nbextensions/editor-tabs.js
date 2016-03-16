1a25521a9a0cca3832240e6bea224d1551a5ad8d
Min RK <benjaminrk@gmail.com>
2016-03-16 15:17:08 +0100

    add editor-tabs

---
+++ nbextensions/editor-tabs.js
@@ -0,0 +1,48 @@
+/*
+Install this with
+
+    jupyter nbextension install editor-tabs.js
+
+Enable it with:
+
+    jupyter nbextension enable --section edit editor-tabs
+
+*/
+define( function () {
+  var $ = require('jquery');
+  var Jupyter = require('base/js/namespace');
+
+  function toggle_tabs() {
+    $("#indent-with-tabs").find('.fa-check').toggle();
+    var current = Jupyter.editor.codemirror.getOption('indentWithTabs');
+    Jupyter.editor.update_codemirror_options({indentWithTabs: !current});
+  };
+
+  function add_button() {
+    var current = Jupyter.editor.codemirror.getOption('indentWithTabs');
+    var entry = $("<li>").attr('id', 'indent-with-tabs');
+    entry.append(
+      $("<a>")
+      .attr('href', '#')
+      .text("Indent with Tabs")
+      .click(toggle_tabs)
+      .append(
+        $("<i>").addClass('fa fa-check')
+      )
+    )
+    if (!current) {
+      entry.find(".fa-check").hide();
+    }
+    $("#edit-menu").append(entry);
+  }
+
+  function load_ipython_extension () {
+    console.log("Loading editor-tabs extension");
+    add_button();
+  };
+
+  return {
+    load_ipython_extension : load_ipython_extension,
+  };
+
+});
