6603fdb0923468c9c4253971e7bab040d8ce0e43
Min RK <benjaminrk@gmail.com>
2017-11-28 14:22:34 +0100

    prettier inorder

--- nbextensions/inorder.js
+++ nbextensions/inorder.js
@@ -20,19 +20,19 @@ install and enable with:
     jupyter nbextension enable inorder
 
 */
-define([], function () {
-  var Jupyter = require('base/js/namespace');
-  var events = require('base/js/events');
-  
+define([], function() {
+  var Jupyter = require("base/js/namespace");
+  var events = require("base/js/events");
+
   function handle_cell_above(cell, idx) {
     // a cell below this one has requested execution
-    if (cell.cell_type != 'code') return Promise.resolve();
-    if (cell.element.hasClass('inorder-error')) {
+    if (cell.cell_type != "code") return Promise.resolve();
+    if (cell.element.hasClass("inorder-error")) {
       // return true means we should prevent future execution
       return Promise.resolve(true);
     }
     f = cell.execute();
-    if (typeof f === 'undefined') {
+    if (typeof f === "undefined") {
       f = Promise.resolve();
     }
     return f;
@@ -41,10 +41,10 @@ define([], function () {
   function handle_cell_below(cell, idx) {
     // a cell above this one has been executed
     // clear its output
-    if (cell.cell_type != 'code') return;
-    
+    if (cell.cell_type != "code") return;
+
     unlock_cell(cell);
-    cell.element.removeClass('inorder-executed');
+    cell.element.removeClass("inorder-executed");
     cell.clear_output();
   }
 
@@ -56,36 +56,35 @@ define([], function () {
     var cells = Jupyter.notebook.get_cells();
     var ncells = nb.ncells();
 
-    if (idx+1 == ncells) return;
+    if (idx + 1 == ncells) return;
 
-    if (cells[idx+1].element.hasClass('inorder-executed'))
-    {
-      cell.element.addClass('inorder-executed');
+    if (cells[idx + 1].element.hasClass("inorder-executed")) {
+      cell.element.addClass("inorder-executed");
       invalidate_below(idx);
     }
   }
-  
+
   function unlock_cell(cell) {
     // unlock a given cell
-    if (cell.cell_type != 'code') return;
-    cell.element.removeClass('inorder-locked');
-    cell.element.removeClass('inorder-error');
+    if (cell.cell_type != "code") return;
+    cell.element.removeClass("inorder-locked");
+    cell.element.removeClass("inorder-error");
     cell.clear_output();
   }
-  
+
   function lock_cell(cell) {
     // lock a cell.
-    if (cell.cell_type != 'code') return;
+    if (cell.cell_type != "code") return;
     // on edit, unlock cell and invalidate cells below
-    cell.element.addClass('inorder-locked');
-    cell.code_mirror.on("change", function () {
+    cell.element.addClass("inorder-locked");
+    cell.code_mirror.on("change", function() {
       unlock_cell(cell);
       var nb = Jupyter.notebook;
       var idx = nb.find_cell_index(cell);
       invalidate_below(idx);
     });
   }
-  
+
   function invalidate_below(idx) {
     // invalidate cells after a given cell.
     var cells = Jupyter.notebook.get_cells();
@@ -93,7 +92,7 @@ define([], function () {
       handle_cell_below(cells[i], i);
     }
   }
-  
+
   function my_execute() {
     // our version of CodeCell.execute
     // check state and maybe restart kernel
@@ -102,45 +101,44 @@ define([], function () {
     var nb = Jupyter.notebook;
     var idx = nb.find_cell_index(cell);
     var cells = nb.get_cells();
-    if (cell.element.hasClass('inorder-locked')) {
+    if (cell.element.hasClass("inorder-locked")) {
       // locked means it's valid, don't re-execute
       return;
     }
-    
+
     // check if cell has been executed
     // if so, restart the kernel and run to here.
-    if (cell.element.hasClass('inorder-executed')) {
+    if (cell.element.hasClass("inorder-executed")) {
       cells.map(unlock_cell);
       console.log("Restarting kernel to restore consistent state.");
       function reexecute(i) {
         // chain re-execute promises
-        console.log('reexecuting', i);
+        console.log("reexecuting", i);
         var promise = nb.get_cell(i).execute();
         if (i < idx) {
-          promise = promise.then(function () {
+          promise = promise.then(function() {
             return reexecute(i + 1);
-          })
+          });
         }
         return promise;
       }
       var resolve_promise, reject_promise;
-      var promise = new Promise(function (resolve, reject){
+      var promise = new Promise(function(resolve, reject) {
         resolve_promise = resolve;
         reject_promise = reject;
       });
 
       nb.kernel.restart(resolve_promise, reject_promise);
-      return promise.then(function () {
+      return promise.then(function() {
         return reexecute(0);
-      })
+      });
     }
-    
+
     // check cells above for
     // - errors
     // - execution
     function chain_above(i) {
-      return handle_cell_above(cells[i], i)
-      .then(function (previous) {
+      return handle_cell_above(cells[i], i).then(function(previous) {
         if (previous === true) {
           // true means error, so stop here
           return previous;
@@ -152,29 +150,29 @@ define([], function () {
       });
     }
     return Promise.resolve()
-    .then(function () {
-      if (idx === 0) return;
-      return chain_above(0);
-    })
-    .then(function (was_error) {
-      if (was_error) return;
-      if (cell.element.hasClass('inorder-locked')) {
-        // locked means it's valid, don't re-execute
-        // check again because this could be delayed
-        // from initial check
-        return;
-      }
+      .then(function() {
+        if (idx === 0) return;
+        return chain_above(0);
+      })
+      .then(function(was_error) {
+        if (was_error) return;
+        if (cell.element.hasClass("inorder-locked")) {
+          // locked means it's valid, don't re-execute
+          // check again because this could be delayed
+          // from initial check
+          return;
+        }
 
-      // actually lock and run this cell
-      cell.element.addClass('inorder-executed');
-      lock_cell(cell);
-      var f = cell.original_execute();
-      // invalidate cells after this one, now that we have been executed
-      invalidate_below(idx);
-      return f; // in case original execute returns a promise
-    });
+        // actually lock and run this cell
+        cell.element.addClass("inorder-executed");
+        lock_cell(cell);
+        var f = cell.original_execute();
+        // invalidate cells after this one, now that we have been executed
+        invalidate_below(idx);
+        return f; // in case original execute returns a promise
+      });
   }
-  
+
   function cell_for_output_area(output_area) {
     // get the cell corresponding to a given output area,
     // since output areas don't have a reference to their parent
@@ -187,7 +185,7 @@ define([], function () {
       }
     }
   }
-  
+
   function handle_error(evt, data) {
     // called when an error is produced by execution
     // triggers invalidation of cells below and prevents their execution
@@ -195,73 +193,71 @@ define([], function () {
     var cell = cell_for_output_area(output_area);
     var nb = Jupyter.notebook;
     var idx = nb.find_cell_index(cell);
-    cell.element.addClass('inorder-error');
+    cell.element.addClass("inorder-error");
     invalidate_below(idx);
   }
-  
+
   function monkeypatch() {
     // patch Cell.execute and OutputArea.append_error for missing functionality
-    
+
     // find the first code cell
     console.log("patching execute");
-    
+
     // create and destroy code cell at end
     var nb = Jupyter.notebook;
     var ncells = nb.ncells();
-    var cell = nb.insert_cell_at_index('code', ncells);
-    
+    var cell = nb.insert_cell_at_index("code", ncells);
+
     var CodeCell = Object.getPrototypeOf(cell);
     var OutputArea = Object.getPrototypeOf(cell.output_area);
     Jupyter.notebook.delete_cells([ncells]);
-    
+
     if (CodeCell.original_execute) {
       // already patched
       return;
     }
     CodeCell.original_execute = CodeCell.execute;
     CodeCell.execute = my_execute;
-    
+
     // patch OutputArea.append_error because there is no event for errors
     OutputArea.original_append_error = OutputArea.append_error;
-    OutputArea.append_error = function () {
+    OutputArea.append_error = function() {
       OutputArea.original_append_error.apply(this, arguments);
-      events.trigger('output_error.InOrderExtension', {
+      events.trigger("output_error.InOrderExtension", {
         output_area: this,
         data: arguments[0],
       });
     };
   }
-  
-  function load_extension () {
+
+  function load_extension() {
     console.log("Loading inorder extension");
 
-	// when a cell is created we check
-	events.on('create.Cell', handle_cell_creation);
+    // when a cell is created we check
+    events.on("create.Cell", handle_cell_creation);
 
     // when there's an error, mark it and invalidate below
-    events.on('output_error.InOrderExtension', handle_error);
+    events.on("output_error.InOrderExtension", handle_error);
 
     // when the kernel restarts, reset all state
-    events.on('kernel_restarting.Kernel', function () {
+    events.on("kernel_restarting.Kernel", function() {
       var cells = Jupyter.notebook.get_cells();
-      cells.map(function (cell) {
+      cells.map(function(cell) {
         unlock_cell(cell);
-        cell.element.removeClass('inorder-executed');
+        cell.element.removeClass("inorder-executed");
       });
     });
-    
+
     // add our css (!important due to race with cell.selected)
     var style = document.createElement("style");
     style.type = "text/css";
-    style.innerHTML = 
-      "div.cell.code_cell.inorder-locked { background-color: #afa}\n" +
-      "div.cell.code_cell.inorder-locked.inorder-error { background-color: #faa}\n"
-    ;
+    style.innerHTML = "div.cell.code_cell.inorder-locked { background-color: #afa}\n" +
+      "div.cell.code_cell.inorder-locked.inorder-error { background-color: #faa}\n";
     document.getElementsByTagName("head")[0].appendChild(style);
-  
+
     // apply patches when the notebook has been loaded
     if (!Jupyter.notebook) {
-      events.one('notebook_loaded.Notebook', monkeypatch);
+      events.one("notebook_loaded.Notebook", monkeypatch);
     } else {
       monkeypatch();
     }
